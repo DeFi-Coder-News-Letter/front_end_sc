24a4aea737eff8c364a0f012f06ea75e73d6c0ce
gaoyang <2313332818@qq.com>
2019-10-27 17:39:32 +0800

    init test.

--- src/ABIs/MoneyMarket.js
+++ src/ABIs/MoneyMarket.js
@@ -9,7 +9,7 @@ export default function MoneyMarket() {
     var NetworkName = findNetwork(window.web3.version.network);
     var MoneyMarket;
     if (NetworkName === 'Main') {
-        MoneyMarket = contractSpec.at("0x0eEe3E3828A45f7601D5F54bF49bB01d1A9dF5ea");
+        MoneyMarket = contractSpec.at("0xeda3849869fd560b49dab8c110be3a020f46c79e");
     } else if (NetworkName === 'Rinkeby') {
         MoneyMarket = contractSpec.at("0x5759F246E6b66B654c61Fec7427dc69E693E98fA");
     }
