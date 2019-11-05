a059423bdda9217dd87569156d02d92d8bdabfa1
gy <curry@gy.com>
2019-11-05 11:31:51 +0800

    update account_supply.js to get status. (2)

--- src/container/accountInfo/accountInfo_supply.js
+++ src/container/accountInfo/accountInfo_supply.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react';
 import InfoSection from '../../component/infoSection/infoSection';
 import Network from '../../constant.json';
-import USDX from "./../../ABIs/USDX.js";
 import MoneyMarket from './../../ABIs/MoneyMarket.js';
-
-import { toFormatShowNumber, getPercentageFormat, toDoubleThousands, findNetwork, formatBigNumber } from '../../util.js';
+import { getPercentageFormat, toDoubleThousands, findNetwork } from '../../util.js';
 import './accountInfo.scss';
 
 // add i18n.
@@ -15,99 +13,11 @@ import zh_CN from '../../language/zh_CN';
 class AccountInfo extends Component {
   constructor(props) {
     super(props)
-    this.state = {
-      borrowBalance: 0,
-      borrowBalanceShow: 0,
-      borrowInterestRate: 0,
-      supplyInterestRate: 0,
-      totalSupply: 0,
-      totalBorrowInMoneyMarket: 0,
-      totalBorrowInMoneyMarketUnformat: 0,
-      totalSupplyUSDxInMoneyMarket: 0,
-      totalSupplyWETHInMoneyMarket: 0,
-      availableToBorrow: 0,
-      collateralRatio: 0,
-      collateralRatioShow: 0,
-      usdxBalanceOf: 0,
-      cash: 0,
-      utilizationRate: 0,
-      usdxAssetPrice: 0,
-      WETHAssetPrice: 0,
-      supplyWETHAmount: 0,
-      originationFee: 0,
-      tatol_Supplied: 0
-    }
+    this.state = {}
     this.web3 = window.web3;
-
-    this.componentDidMount_temp();
-
-    if (window.web3.currentProvider.isMetaMask) {
-      window.ethereum.on('accountsChanged', () => {
-        this.componentDidMount_temp();
-      });
-    }
   }
 
-
-  // ****************** tatol_Supplied    ***************** totalBorrowInMoneyMarket
-  getUtilizationRate = () => {
-    if (USDX() === undefined) {
-      return;
-    }
-    let mmAddress = '';
-    let NetworkName = findNetwork(window.web3.version.network);
-    if (NetworkName === 'Main') {
-      mmAddress = Network.Main.MoneyMarket;
-    } else if (NetworkName === 'Rinkeby') {
-      mmAddress = Network.Rinkeby.MoneyMarket;
-    }
-    // get cash_balance
-    USDX().balanceOf(
-      mmAddress,
-      (err, res) => {
-        let cash = 0;
-        if (res !== undefined && res !== null) {
-          cash = res.toNumber();
-          this.setState({ cash: toFormatShowNumber(this.web3.fromWei(cash, "ether")) }, () => {
-            // setState 成功后回调函数 
-            // console.log('1111111111 ----- this.state.cash: ', this.state.cash);
-            if (MoneyMarket() === undefined) {
-              return;
-            }
-            let usdxAddress = '';
-            let NetworkName = findNetwork(window.web3.version.network);
-            if (NetworkName === 'Main') {
-              usdxAddress = Network.Main.USDx;
-            } else if (NetworkName === 'Rinkeby') {
-              usdxAddress = Network.Rinkeby.USDx;
-            }
-            MoneyMarket().markets(
-              usdxAddress,
-              (err, res) => {
-                if (res !== undefined && res !== null) {
-                  this.setState({
-                    totalBorrowInMoneyMarket: this.web3.fromWei(res[6], "ether"),
-                    totalBorrowInMoneyMarketUnformat: toFormatShowNumber(this.web3.fromWei(res[6].toNumber(), "ether"))
-                  }, () => {
-                    // setState 成功后回调函数 
-                    // console.log('2222222222 -----this.state.totalBorrowInMoneyMarket： ', this.state.totalBorrowInMoneyMarketUnformat);
-                    this.setState({ tatol_Supplied: this.state.totalBorrowInMoneyMarket * 1 + Number(this.state.cash) }, () => {
-                      // console.log('33333333333333 -----this.state.totalBorrowInMoneyMarket： ', this.state.tatol_Supplied);
-                      let UR = 0;
-                      if (Number(this.state.totalBorrowInMoneyMarketUnformat) + Number(this.state.cash) !== 0) {
-                        UR = Number(this.state.totalBorrowInMoneyMarketUnformat) / (Number(this.state.totalBorrowInMoneyMarketUnformat) + Number(this.state.cash))
-                      }
-                      this.setState({ utilizationRate: getPercentageFormat(UR) });
-                    });
-                  });
-                }
-              })
-          })
-        }
-      });
-  }
-
-  get_USDx_supply = () => {
+  get_USDx_supply_borrow = () => {
     let usdxAddress = '';
     let NetworkName = findNetwork(window.web3.version.network);
     if (NetworkName === 'Main') {
@@ -115,79 +25,45 @@ class AccountInfo extends Component {
     } else if (NetworkName === 'Rinkeby') {
       usdxAddress = Network.Rinkeby.USDx;
     }
-    MoneyMarket().markets(usdxAddress, (err, res) => {
+    MoneyMarket().markets.call(usdxAddress, (err, res) => {
       if (res !== undefined && res !== null) {
-        this.setState({ USDx_supply: this.web3.fromWei(res[3], "ether") });
+        // USDx_supply: res[3]
+        // USDx_borrow: res[6]
+        // USDx_supply_APR: res[4]
+        // USDx_borrow_APR: res[7]
+        this.setState({
+          USDx_supply: this.web3.fromWei(res[3], "ether"),
+          USDx_borrow: this.web3.fromWei(res[6], "ether"),
+          USDx_supply_APR: getPercentageFormat(this.web3.fromWei(res[4].toNumber(), "ether") * 86400 * 365 / 15),
+          USDx_borrow_APR: getPercentageFormat(this.web3.fromWei(res[7].toNumber(), "ether") * 86400 * 365 / 15)
+        });
       }
     })
   }
 
-  // ***************** borrowInterestRate
-  getBorrowInterestRate = () => {
-    if (MoneyMarket() === undefined) {
-      return;
-    }
-    let usdxAddress = '';
-    let NetworkName = findNetwork(window.web3.version.network);
-    if (NetworkName === 'Main') {
-      usdxAddress = Network.Main.USDx;
-    } else if (NetworkName === 'Rinkeby') {
-      usdxAddress = Network.Rinkeby.USDx;
-    }
-    MoneyMarket().markets.call(usdxAddress,
-      (err, res) => {
-        if (res !== null && res !== undefined) {
-          this.setState({ borrowInterestRate: getPercentageFormat(this.web3.fromWei(res[7].toNumber(), "ether") * 86400 * 365 / 15) })
-        }
-      }
-    );
-  }
 
-
-  // **************** supplyInterestRate
-  getSupplyInterestRate = () => {
-    if (MoneyMarket() === undefined) {
-      return;
-    }
-    let usdxAddress = '';
-    let NetworkName = findNetwork(window.web3.version.network);
-    if (NetworkName === 'Main') {
-      usdxAddress = Network.Main.USDx;
-    } else if (NetworkName === 'Rinkeby') {
-      usdxAddress = Network.Rinkeby.USDx;
-    }
-    MoneyMarket().markets.call(usdxAddress,
-      (err, res) => {
-        if (res !== undefined && res !== null) {
-          this.setState({ supplyInterestRate: getPercentageFormat(this.web3.fromWei(res[4].toNumber(), "ether") * 86400 * 365 / 15) })
-        }
-      }
-    );
-  }
-
-
-  componentDidMount_temp = () => {
+  componentDidMount = () => {
     if (window.web3) {
       window.web3.currentProvider.enable().then(
-        res => {
-          console.log(this.web3.eth.accounts[0]);
-        }
+        res => { }
       )
     }
-    setTimeout(() => {
-      this.getBorrowInterestRate();
-      this.getSupplyInterestRate();
-      this.getUtilizationRate();
-      this.get_USDx_supply();
-    }, 700);
-    this.getAddressIntervalBorrow = setInterval(() => {
-      if (true) {
-        this.getBorrowInterestRate();
-        this.getSupplyInterestRate();
-        this.getUtilizationRate();
-        this.get_USDx_supply();
-      }
+
+    this.get_USDx_supply_borrow();
+
+    this.timer_supply = setInterval(() => {
+      this.get_USDx_supply_borrow();
     }, 1000 * 15);
+
+    if (window.web3.currentProvider.isMetaMask) {
+      window.ethereum.on('accountsChanged', () => {
+        this.get_USDx_supply_borrow();
+      });
+    }
+  }
+
+  componentWillUnmount = () => {
+    clearInterval(this.timer_supply);
   }
 
 
@@ -200,18 +76,18 @@ class AccountInfo extends Component {
       },
       {
         title: 'Total Borrowed',
-        amount: (typeof web3 === 'undefined' || this.web3.eth.accounts[0] === undefined || !this.props.login) ? '-' : '$' + toDoubleThousands(this.state.totalBorrowInMoneyMarket * 1),
+        amount: (typeof web3 === 'undefined' || this.web3.eth.accounts[0] === undefined || !this.props.login) ? '-' : '$' + toDoubleThousands(this.state.USDx_borrow),
         page: 'lend'
       },
 
       {
         title: 'Supply APR',
-        amount: (typeof web3 === 'undefined' || this.web3.eth.accounts[0] === undefined || !this.props.login) ? '-' : toDoubleThousands(this.state.supplyInterestRate) + '%',
+        amount: (typeof web3 === 'undefined' || this.web3.eth.accounts[0] === undefined || !this.props.login) ? '-' : toDoubleThousands(this.state.USDx_supply_APR) + '%',
         page: 'lend'
       },
       {
         title: 'Borrow APR',
-        amount: (typeof web3 === 'undefined' || this.web3.eth.accounts[0] === undefined || !this.props.login) ? '-' : toDoubleThousands(this.state.borrowInterestRate) + '%',
+        amount: (typeof web3 === 'undefined' || this.web3.eth.accounts[0] === undefined || !this.props.login) ? '-' : toDoubleThousands(this.state.USDx_borrow_APR) + '%',
         page: 'lend'
       }
     ];
